192f94fddaa78059ab13500abf6d916185445a2c
Admin <1049877034@qq.com>
2020-09-10 15:41:48 +0800

    First commit.

---
+++ src/com/ssm/dao/StudentDao.java
@@ -0,0 +1,31 @@
+package com.ssm.dao;
+
+public class StudentDao {
+    private String name;
+    private String id;
+    private String tel;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+}
